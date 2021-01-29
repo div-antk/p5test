1719da1804a5babbbc07d1eb439e903c0d8cd852
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-29 21:11:24 +0900

    矩形を計算して描画する

--- js/sketch.js
+++ js/sketch.js
@@ -11,12 +11,29 @@ class Gen {
       0,0,0,0,0,0,0,0,
     ];
   }
+  // 矩形を描画する
+  draw() {
+    for(let [i,c] of this.cells.entries()) {
+      let x = i%8;
+      let y = floor(i/8);
+
+      // ひとつのセルのキャンバス上での幅を定義
+      let w = height/8;
+
+      // 最初の2つは矩形の左上のx,y座標
+      rect(w*x, w*y, w, w);
+    }
+  }
 }
 
 function setup() {
   createCanvas(480, 480);
   // 背景色黒
   background(0);
+
+  // drow関数の計算
+  let g = new Gen();
+  g.draw();
 }
 
 // 描画
